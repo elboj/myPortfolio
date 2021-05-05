042fe853d41fbaaefc27b6ec33d8ecce298d43fa
elboj <adaja.tobi1@gmail.com>
2021-05-05 15:45:15 +0100

    used import and export statement to make js files more concise

--- app.js
+++ app.js
@@ -1,46 +1,12 @@
 //DATA FOR PORTFOLIO
-const works = [
-  {
-    img: "assets/work-plaqad.jpg",
-    title: "Placad",
-    link: "https://plaqad.com/",
-  },
-  {
-    img: "assets/work-alat.jpg",
-    title: "Alat Foundation",
-    link: "http://alatfoundation.com.ng/",
-  },
-  {
-    img: "assets/work-insw.jpg",
-    title: "Interswitch Recreation",
-    link: "https://interswitch.netlify.app/",
-  },
-  {
-    img: "assets/work-pss.jpg",
-    title: "PSS",
-    link: "http://packetswitchings.com/",
-  },
-  {
-    img: "assets/work-crenet.jpg",
-    title: "Crenet Recreation",
-    link: "https://crenet.netlify.app/",
-  },
-  {
-    img: "assets/work-udemy.jpg",
-    title: "Udemy",
-    link: "#",
-  },
-  {
-    img: "assets/work-react.jpg",
-    title: "React Menu App",
-    link: "https://plaqad.com/",
-  },
-  {
-    img: "assets/work-react-1.jpg",
-    title: "React Review App",
-    link: "https://crenet.netlify.app/",
-  },
-];
+import { works } from "./data.js";
+// console.log(works);
+/**
+ * REFACTORING CREATING A NEW dada.js file to hold all our array data for portfolio
+ * adding type module to app.js in index.html, and then importing {works} from ./data.js
+ * import to use type module, and include extension .js in your import statement
+ */
+
 /**
  * Toggle functionality for main menu
  */
---
+++ data.js
@@ -0,0 +1,42 @@
+export let works = [
+  {
+    img: "assets/work-plaqad.jpg",
+    title: "Placad",
+    link: "https://plaqad.com/",
+  },
+  {
+    img: "assets/work-alat.jpg",
+    title: "Alat Foundation",
+    link: "http://alatfoundation.com.ng/",
+  },
+  {
+    img: "assets/work-insw.jpg",
+    title: "Interswitch Recreation",
+    link: "https://interswitch.netlify.app/",
+  },
+  {
+    img: "assets/work-pss.jpg",
+    title: "PSS",
+    link: "http://packetswitchings.com/",
+  },
+  {
+    img: "assets/work-crenet.jpg",
+    title: "Crenet Recreation",
+    link: "https://crenet.netlify.app/",
+  },
+  {
+    img: "assets/work-udemy.jpg",
+    title: "Udemy",
+    link: "#",
+  },
+  {
+    img: "assets/work-react.jpg",
+    title: "React Menu App",
+    link: "https://plaqad.com/",
+  },
+  {
+    img: "assets/work-react-1.jpg",
+    title: "React Review App",
+    link: "https://crenet.netlify.app/",
+  },
+];
